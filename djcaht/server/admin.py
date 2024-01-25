cad2a9ffafdd4f6de631ae7283230ed44c408895
mahammadmurad <mahammad.muradd@gmail.com>
2024-01-25 13:05:46 +0400

    first commit

---
+++ djcaht/server/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Category, Channel, Server
+
+admin.site.register(Channel)
+admin.site.register(Server)
+admin.site.register(Category)
+
+
+# Register your models here.
